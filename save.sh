ce5fbeb031daa65f5577e37be5457bab6c737b9c
Elvira <Elvira Eulitz>
2020-04-20 22:58:06 +0200

    Saving

---
+++ save.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+git add -A .
+
+save_msg="rebuilding site $(date)"
+git commit -m "Saving"
+
+git commit -m "$save_msg"
+
+printf "\033[0;32mFetching latest state from GitHub...\033[0m\n"
+
+git pull origin master
+
+git push origin dev
