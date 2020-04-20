949614e90f195d95dd4c5e28543013ae7f76da98
Elvira <Elvira Eulitz>
2020-04-20 23:00:45 +0200

    Saving

--- save.sh
+++ save.sh
@@ -11,6 +11,6 @@ git commit -m "Saving"
 git commit -m "$save_msg"
 
 printf "\033[0;32mFetching latest state from GitHub...\033[0m\n"
-
-git pull origin dev && git push origin dev
+git push origin dev
+git pull origin dev
 
