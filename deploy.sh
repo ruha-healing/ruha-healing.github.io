32afaeff0d2cfbeb29cb41421010b5bb554534e3
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-04-22 11:00:44 +0200

    Fine tuned deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+# The following script maintains two branches to deploy the site:
+# First, we branch off to a "deploy" branch where we can savely add the "public" dir
+# Next, we create a fresh "master" branch with only the contents of the "public" dir
+
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
+
+  # Delete old deploy branch
+  git branch -D deploy
+
+  # Create a new deploy branch
+  git checkout -b deploy
+
   # Delete all the old site fragments
   rm -rf ./public
 
@@ -26,6 +37,9 @@ if output=$(git status --porcelain) && [ -z "$output" ]; then
 
   # Force push
   git push -f origin master
+
+  # Change back to the branch where we came from
+  git checkout -
 else 
   # Uncommitted changes
   printf "\033[0;32mNeed to run ./save.sh first\033[0m\n"
